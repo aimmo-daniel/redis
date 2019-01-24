fe9572e0543bbf08a137b37a88b23e0fb55170fe
sangjin0309 <ititech.sj@gmail.com>
2019-01-24 10:23:31 +0900

    Initial commit

---
+++ src/main/java/com/sangjin/redis/account/AccountRepository.java
@@ -0,0 +1,8 @@
+package com.sangjin.redis.account;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AccountRepository extends CrudRepository<Account, String> {
+
+
+}
